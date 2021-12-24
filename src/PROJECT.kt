e2a27e7000fd2d1f760ac4874337ceab9cbffb27
Alex <gorbanev.2001@list.ru>
2021-12-24 18:28:47 +0300

    Коммит Горбанева исправления

--- src/PROJECT.kt
+++ src/PROJECT.kt
@@ -1,37 +1,35 @@
 import java.util.*
 
 data class Item(
-    val Name : String = "None",
-    val Price : Double = 0.0
+    val name : String,
+    val price : Double
 )
 
 data class ItemEntry(
-    val ItemDesc : Item,
-    val Number : Int = 0
+    val itemDesc : Item,
+    val number : Int
 )
 
 sealed class DeliveryMethod {
     object SelfPickup: DeliveryMethod()
     data class CourierDelivery(
-        val Adress: String = "None",
-        val PriceDelivery: Int = 0
+        val adress: String,
+        val priceDelivery: Int
     ) : DeliveryMethod()
 }
 
 data class Order(
-    val ItemInfo : ItemEntry,
-    val Delivery : List<DeliveryMethod> = mutableListOf(),
-    val TotalDiscount : Double = 0.0
+    val itemInfo : ItemEntry,
+    val delivery : DeliveryMethod,
+    val totalDiscount : Double = 0.0
 ) {
     fun getTotalPrice(): Double {
-        val DeliveryString = Delivery.toString()
-        var DeliveryPrice = DeliveryString.substringAfterLast('=').substringBeforeLast(')')
-        var Price : Int
-        if(DeliveryPrice.length>10) Price = 0
-        else Price = DeliveryPrice.toInt()
-        var Discount : Double = 1.0
-        if(TotalDiscount>0) Discount = TotalDiscount/100
-        val total : Double = ((ItemInfo.ItemDesc.Price*ItemInfo.Number)-((ItemInfo.ItemDesc.Price*ItemInfo.Number)*Discount))+Price
+        val deliveryString = delivery.toString()
+        val deliveryPrice = if(delivery is DeliveryMethod.CourierDelivery) delivery.priceDelivery
+        else 0
+        var discount : Double = 1.0
+        if(totalDiscount>0) discount = totalDiscount/100
+        val total : Double = ((itemInfo.itemDesc.price*itemInfo.number)-((itemInfo.itemDesc.price*itemInfo.number)*discount))+deliveryPrice
         return total // calculate full price here
     }
 }
@@ -39,12 +37,10 @@ data class Order(
 
 
 fun main() {
-    val Order1 = Order(ItemEntry(Item("Pods",50.0),10), listOf(DeliveryMethod.CourierDelivery("Kurmangazy",100)),20.0)
-    val Order2 = Order(ItemEntry(Item("Pods",150.0),10), listOf(DeliveryMethod.SelfPickup),20.0)
-    val Order3 = Order(ItemEntry(Item("Pods",200.0),5), listOf(DeliveryMethod.SelfPickup),30.0)
+    val Order1 = Order(ItemEntry(Item("Pods",50.0),10),DeliveryMethod.CourierDelivery("Kurman",10),20.0)
+    val Order2 = Order(ItemEntry(Item("Pods",150.0),10),DeliveryMethod.SelfPickup,20.0)
     println(Order1.getTotalPrice())
     println(Order2.getTotalPrice())
-    println(Order3.getTotalPrice())
 }
 
 
